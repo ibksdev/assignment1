b073e6e2dbc9669dd018fb7050269c557b030653
ibksdev <ibks.dev@gmail.com>
2023-03-23 00:41:37 -0700

    assignment 1 usign function

---
+++ module1_assign1.js
@@ -0,0 +1,13 @@
+
+//assignment 1: 
+
+function multiplicationTable()
+{
+ for ( var i=1;i<=10;i++)
+ {
+    var j= "6 x " + i + " = " + 6*i;
+    console.log(j);
+ }   
+}
+
+multiplicationTable();
